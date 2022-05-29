f88b7973c56fd91730fb3a1d859ec58d0619e5ab
Andrii Rastorhuiev <efinswim@gmail.com>
2022-05-29 14:55:37 +0300

    connect mockapi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
+
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaItem from './components/PizzaItem';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('https://62935339089f87a57abe3300.mockapi.io/pizzas')
+      .then(function (response) {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPizzas(data);
+      });
+  }, [])
+
+
   return (
     <div>
       <div className='wrapper'>
