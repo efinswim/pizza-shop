53494ad89e7e03bbcb1877687e0ff70b968bbe26
Andrii Rastorhuiev <efinswim@gmail.com>
2022-07-13 18:06:59 +0300

    rofl

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { filters } from './slices/filterSlice';
 import filterReducer from './slices/filterSlice'
 
+import cart from './slices/cartSlice'
+
 export const store = configureStore({
-  reducer: { filters: filterReducer },
+  reducer: { 
+    filters: filterReducer,
+    cart
+  },
 });
